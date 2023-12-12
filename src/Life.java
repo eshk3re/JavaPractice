9664ee576a38730df6b59f31d9acba8aefde3055
eshk3re <105351013+eshk3re@users.noreply.github.com>
2023-12-12 16:46:27 +0500

    Update Life.java

--- src/Life.java
+++ src/Life.java
@@ -1,2 +1,2 @@
-// Запись для представления данных о качестве жизни
+// Представления данных о качестве жизни
 public record Life(Country country, double family, double health, double freedom, double trust, double generosity) {}
